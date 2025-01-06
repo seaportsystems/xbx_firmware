e116894b6ba58a997d0f3dfae36fb3918ffe5a1e
Andy Whitman <andy@fathom.fishing>
2025-01-06 12:11:12 -0500

    Updated to add time to each reading using adafruit_datetime library

--- lib/reading.py
+++ lib/reading.py
@@ -1,8 +1,11 @@
+from adafruit_datetime import datetime
+
 class Reading:
     def __init__(self, value, unit, description):
         self.value = value
         self.unit = unit
         self.description = description
+        self.datetime = datetime.now().isoformat()
 
     def __str__(self):
         return f"{self.value} {self.unit} - {self.description}"
