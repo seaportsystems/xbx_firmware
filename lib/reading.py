33aeff0b873cef2e6cfcb852294702d6341c002a
Andy Whitman <andy@fathom.fishing>
2025-01-06 12:12:53 -0500

    fixed reading.py to use built in time library instead of attempting to import adafruit_datetime

--- lib/reading.py
+++ lib/reading.py
@@ -1,11 +1,17 @@
-from adafruit_datetime import datetime
+import time
+
+def struct_time_to_iso8601(struct_time):
+    year, month, day, hour, minute, second, _, _, _ = struct_time
+
+    # Format to ISO8601 using f-string
+    return f"{year:04d}-{month:02d}-{day:02d}T{hour:02d}:{minute:02d}:{second:02d}"
 
 class Reading:
     def __init__(self, value, unit, description):
         self.value = value
         self.unit = unit
         self.description = description
-        self.datetime = datetime.now().isoformat()
+        self.datetime = struct_time_to_iso8601(time.localtime())
 
     def __str__(self):
         return f"{self.value} {self.unit} - {self.description}"
