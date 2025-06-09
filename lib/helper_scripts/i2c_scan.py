f4adff8e80f27db5fa88d76d6259350342d9f557
Andy Whitman <andyvwhitman@gmail.com>
2025-06-08 22:15:30 -0400

    added i2c_scan helper script

---
+++ lib/helper_scripts/i2c_scan.py
@@ -0,0 +1,33 @@
+'''
+run this script to see all the devices recognized on the I2C bus
+to run this script, go to the REPL and execute the following command:
+from helper_scripts import i2c_scan
+'''
+
+import board
+from busio import I2C
+
+print("Initializing I2C Bus")
+i2c_bus = I2C(scl=board.GP13, sda=board.GP12)
+
+locked = False
+while(not(locked)):
+    print("Waiting for bus lock")
+    locked = i2c_bus.try_lock()
+    
+print("Bus locked successfully")
+print("Scanning bus")
+i2c_devices = i2c_bus.scan()
+
+print(f"{len(i2c_devices)} devices on the I2C bus")
+print("")
+print("Devices")
+print("-------")
+
+for d in i2c_devices:
+    print(f"{hex(d)} | {d}")
+    
+print("Unlocking bus")
+i2c_bus.unlock()
+print("Deinitializing bus")
+i2c_bus.deinit()
